7b094c2a832b6730fd6d16b34cb9ae1b7a71b797
P-ule-P <ppule1@gmail.com>
2025-05-04 07:22:41 +0200

    Added scripts.js

---
+++ scripts.js
@@ -0,0 +1,59 @@
+// Task 1: Create a task with title, description, and status (todo, doing, done)
+let task1 = prompt("Enter task 1 title:");
+let description1 = prompt("Enter task 1 description:");
+
+let status1;
+while (true) {
+  status1 = prompt("Enter task 1 status (todo, doing, done):");
+  let statusCase = status1.toLowerCase();
+
+  if (statusCase == "todo" || statusCase == "doing" || statusCase == "done") {
+    if (statusCase === "todo") {
+      alert("No tasks completed, let's go to work!");
+      console.log("No tasks completed, let's go to work!");
+      break;
+    } else if (statusCase === "doing") {
+      alert("No tasks completed, let's go to work!");
+      console.log("No tasks completed, let's go to work!");
+      break;
+    } else if (statusCase === "done") {
+      alert(task1 + ": " + description1 + "," + " Status: done");
+      console.log(task1 + ": " + description1 + "," + " Status: done");
+      break;
+    }
+  } else {
+    alert("Invalid status, please enter: 'todo', 'doing', or 'done'.");
+  }
+}
+
+// Task 2: Create a task with title, description, and status (todo, doing, done)
+let task2 = prompt("Enter task 2 title:");
+let description2 = prompt("Enter task 2 description:");
+
+let status2;
+while (true) {
+  status2 = prompt("Enter task 2 status (todo, doing, done):");
+  let statusCases = status2.toLowerCase();
+
+  if (
+    statusCases == "todo" ||
+    statusCases == "doing" ||
+    statusCases == "done"
+  ) {
+    if (statusCases === "todo") {
+      alert("No tasks completed, let's go to work!");
+      console.log("No tasks completed, let's go to work!");
+      break;
+    } else if (statusCases === "doing") {
+      alert("No tasks completed, let's go to work!");
+      console.log("No tasks completed, let's go to work!");
+      break;
+    } else if (statusCases === "done") {
+      alert(task2 + ": " + description2 + "," + " Status: done");
+      console.log(task2 + ": " + description2 + "," + " Status: done");
+      break;
+    }
+  } else {
+    alert("Invalid status, please enter: 'todo', 'doing', or 'done'.");
+  }
+}
